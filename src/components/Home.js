615e9e2f902ee2baf6b48be6202c176c6d3bf8e5
sasha4ox <sasha4ox@gmail.com>
2020-01-19 20:34:21 +0200

    add new img

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Slider from '@farbenmeer/react-spring-slider';
+import main from '../img/Main.jpg';
+import main2 from '../img/main2.jpg';
+import main3 from '../img/main3.jpg';
+import FoxDeli from '../img/FoxDeli.JPG';
 
 function Home() {
   return (
@@ -8,36 +12,20 @@ function Home() {
         <div className={'wrapper__slider'}>
           <Slider auto={5000}>
             <div className={'home__img__wrapper'}>
-              <h2>Мы открыты, для Вас</h2>
-              <img
-                className={'home__img'}
-                src="https://firebasestorage.googleapis.com/v0/b/hotel-a7493.appspot.com/o/413882451.jpg?alt=media&token=252a391e-540b-4bb2-8217-a8e5ecb07986"
-                alt="main"
-              />
+              {/* <h2>Мы открыты, для Вас</h2> */}
+              <img className={'home__img'} src={main} alt="main" />
             </div>
             <div className={'home__img__wrapper'}>
               <h2>Мы работаем, Вы отдыхаете</h2>
-              <img
-                className={'home__img'}
-                src="https://firebasestorage.googleapis.com/v0/b/hotel-a7493.appspot.com/o/413882502.jpg?alt=media&token=e710fa97-f420-4128-965d-3d36b68c256b"
-                alt="main"
-              />
+              <img className={'home__img'} src={main2} alt="main" />
             </div>
             <div className={'home__img__wrapper'}>
               <h2>Мы создаём уют, для Вас</h2>
-              <img
-                className={'home__img'}
-                src="https://firebasestorage.googleapis.com/v0/b/hotel-a7493.appspot.com/o/140464611.jpg?alt=media&token=e4ffb1de-662f-41d1-8a4f-3ea24714804d"
-                alt="main"
-              />
+              <img className={'home__img'} src={main3} alt="main" />
             </div>
             <div className={'home__img__wrapper'}>
               <h2>Мы готовим, для Вас</h2>
-              <img
-                className={'home__img'}
-                src="https://firebasestorage.googleapis.com/v0/b/hotel-a7493.appspot.com/o/419175846.jpg?alt=media&token=c5517907-cc1d-4ca2-a72f-2a605ad55d07"
-                alt="main"
-              />
+              <img className={'home__img'} src={FoxDeli} alt="main" />
             </div>
           </Slider>
         </div>
